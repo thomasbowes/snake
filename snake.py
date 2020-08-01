317e1d0da6509fe85ec83e8e6551f6d25e36f9fc
Thomas Bowes <thomasbowes@Thomass-MacBook-Pro.local>
2020-08-02 02:46:39 +1000

    Added scaffolding for program

--- snake.py
+++ snake.py
@@ -1,1 +1,50 @@
-print("hello world")
+# imports
+import pygame
+import random
+import sys
+
+# Global Variables
+SCREEN_SIZE = (495, 495)
+
+BLOCK_WIDTH = 15
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# Snake
+class snake(object):
+    def __init__(self):
+        pass
+    def get_pos(self):
+        pass
+    def turn(self):
+        pass
+    def move(self):
+        pass
+    def reset(self):
+        pass
+    def draw(self):
+        pass
+    def handle_keys(self):
+        pass
+
+class tail(object):
+    pass
+
+class snack(object):
+    def __init__(self):
+        pass
+    def draw(self, surface):
+        pass
+    def new_random_postion(self):
+        pass
+
+class board(object)
+    pass
+
+def main():
+    pass
+
+main()
